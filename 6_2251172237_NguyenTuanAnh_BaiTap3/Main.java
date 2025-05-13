3a7784bed92ef1a2a35d18e4f66b6b259c6644c5
tuanh2401 <nguyentuananh240104@gmail.com>
2025-05-13 19:47:38 +0700

    nopbtvn

---
+++ 6_2251172237_NguyenTuanAnh_BaiTap3/Main.java
@@ -0,0 +1,52 @@
+package Btap3;
+
+abstract class Vehicle {
+ public abstract int getMaxSpeed();
+}
+
+class Car extends Vehicle {
+ @Override
+ public int getMaxSpeed() {
+     return 200;
+ }
+}
+
+class Bicycle extends Vehicle {
+ @Override
+ public int getMaxSpeed() {
+     return 30;
+ }
+}
+
+abstract class VehicleFactory {
+ public abstract Vehicle createVehicle();
+}
+
+class CarFactory extends VehicleFactory {
+ @Override
+ public Vehicle createVehicle() {
+     return new Car();
+ }
+}
+
+class BicycleFactory extends VehicleFactory {
+ @Override
+ public Vehicle createVehicle() {
+     return new Bicycle();
+ }
+}
+
+public class Main {
+ public static void main(String[] args) {
+     VehicleFactory carFactory = new CarFactory();
+     VehicleFactory bicycleFactory = new BicycleFactory();
+
+     Vehicle[] vehicles = new Vehicle[2];
+     vehicles[0] = carFactory.createVehicle();
+     vehicles[1] = bicycleFactory.createVehicle();
+
+     for (Vehicle v : vehicles) {
+         System.out.println("Tốc độ tối đa: " + v.getMaxSpeed());
+     }
+ }
+}
